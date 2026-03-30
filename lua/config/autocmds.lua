36a97ae18a4a50b4b4758992c358ec9ae3c88206
Allison Bianchi <allisonb@pixar.com>
2026-03-30 15:01:29 -0700

    Tweak Ouroboros keymap

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,9 +5,12 @@ local customAutos = vim.api.nvim_create_augroup("CustomAutos", { clear = true })
 -- autocmd! Filetype c,cpp map<buffer> <C-e> :Ouroboros<CR>
 vim.api.nvim_create_autocmd("FileType", {
     group = customAutos,
-    buffer = 0,
-    desc = "Ouroboros toggle hotkey",
+    buffer = 0, -- FIXME what does this do?
+    desc = "Set up Ouroboros toggle hotkey",
     callback = function()
-        vim.keymap.set('n', '<C-e>', ':Ouroboros<cr>', { desc = 'Toggle between header and implementation' })
+        vim.keymap.set('n', '<C-e>', ':Ouroboros<cr>', {
+            desc = 'Toggle between header and implementation',
+            buffer = 0 -- FIXME doesn't seem to work?
+        })
     end
 })
