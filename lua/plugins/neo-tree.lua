d315d57c60842dbb3f42ba8fb93c30a08c6365a4
Allison Bianchi <allisonb@pixar.com>
2026-04-01 14:42:10 -0700

    Neotree config and window-picker plugin

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,10 +1,44 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons", -- optional, but recommended
+        'nvim-lua/plenary.nvim',
+        'MunifTanjim/nui.nvim',
+        'nvim-tree/nvim-web-devicons', -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
+    config = function()
+        require('neo-tree').setup({
+            close_if_last_window = true,
+            buffers = {
+                window = {
+                    mappings = {
+                        ["f"] = { "open", config = { close_window = true } },
+                    },
+                },
+            },
+        })
+
+        -- Show/focus
+        vim.keymap.set(
+            'n', '<tab>',
+            function()
+                vim.cmd('Neotree left last')
+            end
+        )
+        -- Close
+        vim.keymap.set(
+            'n', '<S-tab>',
+            function()
+                vim.cmd('Neotree close')
+            end
+        )
+        -- Buffer quick-switch
+        vim.keymap.set(
+            'n', '<leader>e',
+            function()
+                vim.cmd('Neotree float source=buffers')
+            end
+        )
+    end
 }
